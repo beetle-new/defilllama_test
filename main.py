d377b1bbdd08cb9e07356d3f7f53cae4f8a8aabe
beetle <121641549+beetle-new@users.noreply.github.com>
2023-03-17 10:59:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import pandas as pd
 import altair as alt
 
 # Set page configuration
-st.set_page_config(
-    page_title="DeFiLlama TVL",
-    page_icon="💰",
-    layout="wide",
-    initial_sidebar_state="collapsed",
-)
+st.set_page_config(page_title="DeFiLlama TVL", page_icon="💰", layout="wide", initial_sidebar_state="collapsed",)
 
 # Set up the API endpoint URL
 url = 'https://api.flipsidecrypto.com/api/v2/queries/456bf6fe-078e-4cbb-987c-c3fbd6621202/data/latest'
@@ -35,7 +30,7 @@ if response.status_code == 200:
             color=alt.Color('CHAIN:N', legend=None)
         ).properties(
             title=title.title() + ' Change',
-            width=800,
+            width=500,
             height=500
         )
         # Create the data table
@@ -52,7 +47,7 @@ if response.status_code == 200:
 
 # Set the app header
 st.title("DeFi TVL Tracker")
-st.write("Welcome to our DeFi TVL tracker! Our tool provides real-time insights into the Total Value Locked (TVL) of various DeFi protocols on Ethereum, Binance Smart Chain, Tron, and Arbitrum. Stay up-to-date with the latest DeFi trends and make informed investment decisions with our DeFi TVL tracker.")
+st.write("Welcome to our DeFi TVL tracker! This tool provides real-time insights into the Total Value Locked (TVL) in USD within various DeFi protocols on Ethereum, Binance Smart Chain, Tron, and Arbitrum. Stay up-to-date with the latest DeFi trends and make informed investment decisions with our DeFi TVL tracker.")
 st.write("Created by @1kbeetlejuice for Derek")
 # Add some space for better readability
 st.write("")
@@ -67,19 +62,29 @@ monthly_max = monthly_data.loc[monthly_data['MONTHLY_CHANGE'].idxmax()]
 
 
 # Display the changes with the most negative and positive values for each timeframe
-st.metric("Weekly Change", f"{weekly_min['CHAIN']}: {weekly_min['WEEKLY_CHANGE']:,.2f} (Most Negative) | {weekly_max['CHAIN']}: {weekly_max['WEEKLY_CHANGE']:,.2f} (Most Positive)")
-st.metric("Monthly Change", f"{monthly_min['CHAIN']}: {monthly_min['MONTHLY_CHANGE']:,.2f} (Most Negative) | {monthly_max['CHAIN']}: {monthly_max['MONTHLY_CHANGE']:,.2f} (Most Positive)")
+beetlecol1, beetlecol2 = st.columns(2)
+with beetlecol1:
+    st.write(f"<h3 style='color: green;'>Largest Increase in Weekly TVL</h3><h4>{weekly_max['CHAIN']}: ${weekly_max['WEEKLY_CHANGE']:,.1f}</h4>", unsafe_allow_html=True)
+with beetlecol2:
+    st.write(f"<h3 style='color: red;'>Largest Decrease in Weekly TVL</h3><h4>{weekly_min['CHAIN']}: ${weekly_min['WEEKLY_CHANGE']:,.1f}</h4>", unsafe_allow_html=True)
 
+nikkicol1, nikkicol2 = st.columns(2)
+with nikkicol1:
+    st.write(f"<h3 style='color: green;'>Largest Increase in Monthly TVL</h3><h4>{monthly_max['CHAIN']}: ${monthly_max['MONTHLY_CHANGE']:,.1f}</h4>", unsafe_allow_html=True)
+with nikkicol2:
+    st.write(f"<h3 style='color: red;'>Largest Decrease in Monthly TVL</h3><h4>{monthly_min['CHAIN']}: ${monthly_min['MONTHLY_CHANGE']:,.1f}</h4>", unsafe_allow_html=True)
 
-tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-tab1.subheader("Weekly Change")
-tab1.altair_chart(weekly_chart)
-tab2.subheader("Weekly Change Data")
-tab2.write(weekly_data)
+ncol1, ncol2 = st.columns(2)
+with ncol1:
+    tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+    tab1.subheader("Weekly Change")
+    tab1.altair_chart(weekly_chart)
+    tab2.subheader("Weekly Change Data")
+    tab2.write(weekly_data)
     
-
-tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-tab1.subheader("Monthly Change")
-tab1.altair_chart(monthly_chart)
-tab2.subheader("Monthly Change Data")
-tab2.write(monthly_data)
+with ncol2:
+    tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+    tab1.subheader("Monthly Change")
+    tab1.altair_chart(monthly_chart)
+    tab2.subheader("Monthly Change Data")
+    tab2.write(monthly_data)
